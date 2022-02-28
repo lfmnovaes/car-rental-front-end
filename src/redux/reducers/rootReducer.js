58caa46f3f99ac9542182a5b777d893859691583
enmanuel <enmanuellassis@gmail.com>
2022-02-28 15:14:17 -0400

    add user reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import usersReducer from './usersReducers';
 
 const rootReducer = combineReducers({
   // greetings: greetingsReducer,
+  users: usersReducer,
 });
 
 export default rootReducer;
