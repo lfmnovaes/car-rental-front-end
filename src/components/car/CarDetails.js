ad56d831bf5cd60ea5c5e0221743d630f2468215
enmanuel <enmanuellassis@gmail.com>
2022-03-01 20:54:31 -0400

    fixing styling issue

--- src/components/car/CarDetails.js
+++ src/components/car/CarDetails.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getCar } from '../../redux/reducers/carsReducer';
 import PopUp from './PopUp';
+import './Details.css';
 
 const CarDetails = () => {
   const dispatch = useDispatch();
@@ -26,12 +27,12 @@ const CarDetails = () => {
   const button = (reserved === 'no') ? <button type="button" onClick={() => navigate('/reserve')} className="reserve-button p-3 w-40 rounded-3xl text-center mt-5 bg-blue-600 hover:bg-blue-500 capitalize text-white">reserve</button> : <button type="button" className="p-3 w-40 rounded-3xl text-center mt-5 bg-gray-200 text-gray-500 " disabled>Reserved</button>;
   return (
     <>
-      <PopUp carId={id} />
-      <div className="car-container sm:flex sm:gap-5 md:justify-center">
-        <div className="img-container sm:items-center sm:w-1/2 justify-center">
+      <PopUp />
+      <div className="car-container lg:w-8/12">
+        <div className="img-container">
           <img src={carSelected.image} alt="car-img" className="w-100" />
         </div>
-        <div className="details flex flex-col items-center sm:w-1/3 lg:mt-10">
+        <div className="details sm:w-52 sm:mx-auto">
           <div className="title flex gap-1 uppercase font-bold">
             <p>{ carSelected.brand }</p>
             <p>{ carSelected.model }</p>
@@ -49,7 +50,7 @@ const CarDetails = () => {
               <p className="capitalize px-2">reserved:</p>
               <p className="capitalize px-0">{ reserved }</p>
             </li>
-            <li className="btn flex flex-col">
+            <li className="btn flex flex-col items-center">
               { button }
               <button type="button" className="reserve-button p-3 w-40 rounded-3xl text-center mt-5 bg-red-500 hover:bg-red-400 capitalize text-white" onClick={() => { showPopup(); }}>delete</button>
             </li>
