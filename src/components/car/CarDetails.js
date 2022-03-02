4abdbae790c2893a03708f13cff426062144f27c
enmanuel <enmanuellassis@gmail.com>
2022-03-01 21:27:16 -0400

    Add back button to details

--- src/components/car/CarDetails.js
+++ src/components/car/CarDetails.js
@@ -24,38 +24,41 @@ const CarDetails = () => {
 
   const carSelected = useSelector((state) => state.car);
   const reserved = (carSelected.reserved === false) ? 'no' : 'yes';
-  const button = (reserved === 'no') ? <button type="button" onClick={() => navigate('/reserve')} className="reserve-button p-3 w-40 rounded-3xl text-center mt-5 bg-blue-600 hover:bg-blue-500 capitalize text-white">reserve</button> : <button type="button" className="p-3 w-40 rounded-3xl text-center mt-5 bg-gray-200 text-gray-500 " disabled>Reserved</button>;
+  const button = (reserved === 'no') ? <button type="button" onClick={() => navigate('/reserve')} className="reserve-button green-bg p-3 w-40 rounded-3xl text-center mt-5 capitalize text-white">reserve</button> : <button type="button" className="p-3 w-40 rounded-3xl text-center mt-5 bg-gray-200 text-gray-500 " disabled>Reserved</button>;
   return (
     <>
       <PopUp />
-      <div className="car-container lg:w-8/12">
-        <div className="img-container">
-          <img src={carSelected.image} alt="car-img" className="w-100" />
-        </div>
-        <div className="details sm:w-52 sm:mx-auto">
-          <div className="title flex gap-1 uppercase font-bold">
-            <p>{ carSelected.brand }</p>
-            <p>{ carSelected.model }</p>
+      <div className="flex flex-col">
+        <div className="car-container">
+          <div className="img-container">
+            <img src={carSelected.image} alt="car-img" className="w-100" />
           </div>
-          <ul className="data mt-2">
-            <li className="flex bg-gray-200">
-              <p className="capitalize px-2">year:</p>
-              <p className="capitalize px-7">{ carSelected.year }</p>
-            </li>
-            <li className="flex">
-              <p className="capitalize px-2">price:</p>
-              <p className="capitalize px-6">{ carSelected.price }</p>
-            </li>
-            <li className="flex bg-gray-200">
-              <p className="capitalize px-2">reserved:</p>
-              <p className="capitalize px-0">{ reserved }</p>
-            </li>
-            <li className="btn flex flex-col items-center">
-              { button }
-              <button type="button" className="reserve-button p-3 w-40 rounded-3xl text-center mt-5 bg-red-500 hover:bg-red-400 capitalize text-white" onClick={() => { showPopup(); }}>delete</button>
-            </li>
-          </ul>
+          <div className="details sm:w-52 sm:mx-auto">
+            <div className="title flex gap-1 uppercase font-bold">
+              <p>{ carSelected.brand }</p>
+              <p>{ carSelected.model }</p>
+            </div>
+            <ul className="data mt-2">
+              <li className="flex bg-gray-200">
+                <p className="capitalize px-2">year:</p>
+                <p className="capitalize px-7">{ carSelected.year }</p>
+              </li>
+              <li className="flex">
+                <p className="capitalize px-2">price:</p>
+                <p className="capitalize px-6">{ carSelected.price }</p>
+              </li>
+              <li className="flex bg-gray-200">
+                <p className="capitalize px-2">reserved:</p>
+                <p className="capitalize px-0">{ reserved }</p>
+              </li>
+              <li className="btn flex flex-col items-center">
+                { button }
+                <button type="button" className="reserve-button p-3 w-40 rounded-3xl text-center mt-5 bg-red-500 hover:bg-red-400 capitalize text-white" onClick={() => { showPopup(); }}>delete</button>
+              </li>
+            </ul>
+          </div>
         </div>
+        <button type="button" className="green-bg p-3 w-20 rounded-3xl rounded-l-none text-center mt-5 capitalize text-white" onClick={() => { navigate('/cars'); }}>&#10092;</button>
       </div>
     </>
   );
